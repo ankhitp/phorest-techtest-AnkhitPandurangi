b94e13915502f7231b7591b7c89feb111c1397fe
Ankhit <aapandurangi@ucsd.edu>
2019-10-05 16:20:17 +0100

    all functionality working, working on styling

--- run.py
+++ run.py
@@ -12,15 +12,29 @@ def index():
 def getClientData():
     url = "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?"
     clientInfo = request.form['clientInfo']
-    if "@" in clientInfo:
-        url += "email="
-        clientInfo = clientInfo.replace("@", "%40")
-        url += clientInfo+"%20&page=0&size=20"
-        url = url.replace(" ", "")
-        print(url)
-        r = requests.get(url, auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
-        r = r.json()
-        return json.dumps(r)
+    type = request.form['type']
+    if type == "email":
+        if "@" not in clientInfo:
+            return "-10"
+        else:
+            url += "email="
+            clientInfo = clientInfo.replace("@", "%40")
+            url += clientInfo+"%20&page=0&size=20"
+            url = url.replace(" ", "")
+            r = requests.get(url, auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
+            r = r.json()
+            return json.dumps(r)
+    else:
+        try:
+            testPhone = int(clientInfo)
+            url += "page=0&phone=" +clientInfo+"&size=20"
+            url = url.replace(" ", "")
+            r = requests.get(url, auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
+            r = r.json()
+            return json.dumps(r)
+        except ValueError:
+            return "-20"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000,debug=True)
